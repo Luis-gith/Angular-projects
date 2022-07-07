f389e7d4d8ac51c209eb8ccdc68a5af10ec4852f
Luis-gith <luisfelipebelasco@hotmail.com>
2022-07-07 18:02:13 -0300

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,42 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { RouterModule, Routes } from '@angular/router';
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+
+
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full'}
+];
 
 @NgModule({
+
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
+   ],
+
+  imports:[
+     BrowserModule,
+     RouterModule.forRoot(routes),
+     AppRoutingModule,
+     BrowserAnimationsModule,
+     MatToolbarModule,    
+     MatIconModule, 
   ],
-  imports: [
-    BrowserModule
-  ],
+
   providers: [],
-  bootstrap: [AppComponent]
+
+  bootstrap: [
+     AppComponent
+   ],
 })
+
+
 export class AppModule { }
